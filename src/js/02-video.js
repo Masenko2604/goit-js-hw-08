c741bc8724daddbf81ecdaffbbed976cf7c6e5ed
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2023-11-18 22:32:05 +0100

    тз - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
-// Отримуємо відеоелемент
 const videoPlayer = document.getElementById('vimeo-player');
 
-// Перевіряємо, чи підтримує браузер локальне сховище
 if (typeof(Storage) !== "undefined") {
-  // Встановлюємо обробник події timeupdate, щоб оновлювати час відтворення
-  videoPlayer.addEventListener('timeupdate', _.throttle(function() {
-    // Оновлюємо поточний час відтворення у локальному сховищі
+ 
+  videoPlayer.addEventListener('timeupdate', throttle(function() {
+    
     localStorage.setItem('videoplayer-current-time', videoPlayer.currentTime);
-  }, 1000)); // Викликаємо функцію не частіше, ніж раз на секунду
-
-  // Перевіряємо, чи є збережений час відтворення
+  }, 1000));  
   const currentTime = localStorage.getItem('videoplayer-current-time');
   if (currentTime) {
-    // Відновлюємо відтворення з збереженого часу
-    videoPlayer.currentTime = currentTime;
+   
+    videoPlayer.currentTime = videoPlayer.setCurrentTime(currentTime);
   }
 } else {
   alert('Ваш браузер не підтримує локальне сховище.');
